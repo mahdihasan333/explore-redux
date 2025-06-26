7f75d600670225b5a24326d3d1c6b9a776aa2617
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-27 01:23:34 +0600

    Understanding Mutation And Ways To Avoid It

--- redux-bootstrap/test.js
+++ redux-bootstrap/test.js
@@ -1,27 +1,62 @@
+import {produce} from 'immer'
+
 //* pure function
 
-const add = (a, b) => a + b;
+// const add = (a, b) => a + b;
 
 // console.log(add(0, 1))
 // console.log(add(1, 1))
 // console.log(add(2, 1))
 
 //* Impure function
-let total = 0
-const addToTotal = (amount) => (total = total + amount)
+// let total = 0
+// const addToTotal = (amount) => (total = total + amount)
 
-console.log(addToTotal(4))
+// console.log(addToTotal(4))
 
 
-const updateDate = () => {
-    new Date()
+// const updateDate = () => {
+//     new Date()
+// }
+
+// const randomNumber = (amount) => {
+//     return amount + Math.random()
+// }
+
+// console.log(randomNumber(3))
+// console.log(randomNumber(3))
+// console.log(randomNumber(3))
+// console.log(randomNumber(3))
+
+
+//* -----------------------------------
+
+// const employee = {
+//     name: 'Mahdi',
+//     address: {country: 'Bangladesh', city: 'Lakhsmipur'}
+// }
+
+// const employee2 = {
+//     ...employee,
+//     name: 'Hassan',
+//     address: {...employee.address, city: 'Chittagong'}
+// }
+
+// console.log(employee == employee2)
+// console.log(employee)
+// console.log(employee2)
+
+
+//* --------------------------
+const employee = {
+    name: 'Mahdi',
+    address: {country: 'Bangladesh', city: 'Lakhsmipur'}
 }
 
-const randomNumber = (amount) => {
-    return amount + Math.random()
-}
+const employee2 = produce(employee, (draft)=> {
+    draft.name = "Hassan",
+    draft.address.city = "Chittagong"
+})
 
-console.log(randomNumber(3))
-console.log(randomNumber(3))
-console.log(randomNumber(3))
-console.log(randomNumber(3))
+console.log(employee)
+console.log(employee2)
