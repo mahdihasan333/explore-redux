69f027afdb1a18ff5d1990426dbe1297a5e7ee94
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-28 16:26:16 +0600

    Configure Basic Routing Using React-Router-Dom

--- redux-todo-app/src/App.tsx
+++ redux-todo-app/src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router";
+import Navbar from "./components/layout/Navbar";
 import { Button } from "./components/ui/button";
 
-function App(){
-  return <Button>Test</Button>
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
---
+++ redux-todo-app/src/pages/Tasks.tsx
@@ -0,0 +1,7 @@
+export default function Tasks() {
+  return (
+    <div>
+      <h1>This is Tasks component</h1>
+    </div>
+  )
+}
