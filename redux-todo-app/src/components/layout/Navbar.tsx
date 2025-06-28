2cebdaa296a8a5c1051acc93b3cf2b329703c960
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-28 17:50:25 +0600

    Handel Dark Mode Using Shadcn

--- redux-todo-app/src/components/layout/Navbar.tsx
+++ redux-todo-app/src/components/layout/Navbar.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggler";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+    <nav className="max-w-7xl mx-auto h-16 flex justify-between items-center gap-3 px-5">
       <div className="flex items-center">
         <span className="font-bold ml-2">Task</span>Master
       </div>
-      <Link to='/'>Tasks</Link>
-      <Link to='/users'>User</Link>
+
+      <Link to="/">Tasks</Link>
+      <Link to="/users">User</Link>
+
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
