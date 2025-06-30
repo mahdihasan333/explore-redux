11c02c20257afb4a3b44198d271d297d8983d9cc
Mahdi Hassan <mahdioffi333@gmail.com>
2025-06-30 18:50:38 +0600

    Implementing Task Addition

--- redux-todo-app/src/redux/features/task/taskSlice.ts
+++ redux-todo-app/src/redux/features/task/taskSlice.ts
@@ -1,47 +1,43 @@
 import type { RootState } from "@/redux/store";
 import type { ITask } from "@/types";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
 
-interface InitialState{
-    task: ITask[];
-    filter: 'all' | 'high' | 'medium' | 'low';
+interface InitialState {
+  task: ITask[];
+  filter: "all" | "high" | "medium" | "low";
 }
 
 const initialState = {
-  tasks: [
-    {
-      id: "adkjadkfja",
-      title: "Initialize frontend",
-      description: "Create Home page, and routing",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "High",
-    },
-    {
-      id: "adkjadkfjasadasfasf",
-      title: "init github repo",
-      description: "Create stage branch",
-      dueDate: "2025-11",
-      isCompleted: false,
-      priority: "Medium",
-    },
-  ],
-  filter: 'all'
+  tasks: [],
+  filter: "all",
 };
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
-  reducers: {},
+  reducers: {
+    addTask: (state, action: PayloadAction<ITask>) => {
+      const id = uuidv4();
+      const taskData = {
+        ...action.payload,
+        id,
+        isCompleted: false,
+      };
+
+      state.tasks.push(taskData);
+    },
+  },
 });
 
 export const selectTasks = (state: RootState) => {
   return state.todo.tasks;
-}
-
+};
 
 export const selectFilter = (state: RootState) => {
   return state.todo.filter;
-}
+};
+
+export const { addTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
