da0ab414ec638f923632e5a2371e04f4bf4fc305
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-01 15:07:25 +0600

    Creating Task Filters

--- redux-todo-app/src/components/module/tasks/taskCard.tsx
+++ redux-todo-app/src/components/module/tasks/taskCard.tsx
@@ -1,40 +1,54 @@
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
-import { deleteTask, toggleCompleteState } from "@/redux/features/task/taskSlice";
+import {
+  deleteTask,
+  toggleCompleteState,
+} from "@/redux/features/task/taskSlice";
 import { useAppDispatch } from "@/redux/hook";
 import type { ITask } from "@/types";
 
 import { Trash2 } from "lucide-react";
 
-interface IProps{
-    task: ITask;
+
+interface IProps {
+  task: ITask;
 }
 
-export default function TaskCard({task}: IProps){
+export default function TaskCard({ task }: IProps) {
+  const dispatch = useAppDispatch();
 
-
-    const dispatch = useAppDispatch()
-
-    return (
-        <div className="border px-5 py-3 rounded-md">
-            <div className="flex justify-between items-center">
-                <div className="flex gap-2 items-center">
-                    <div className={cn("size-3 rounded-full", {
-                        'bg-red-500': task.priority === 'high',
-                        'bg-yellow-500': task.priority === 'medium',
-                        'bg-green-500': task.priority === 'low',
-                    })}></div>
-                    <h1 className={cn({'line-through': task.isCompleted})}>{task.title}</h1>
-                </div>
-                <div className="flex gap-3 items-center">
-                    <Button onClick={() => dispatch(deleteTask(task.id))}  variant='link' className="p-0 text-red-500">
-                        <Trash2/>
-                    </Button>
-                    <Checkbox checked={task.isCompleted} onClick={() => dispatch(toggleCompleteState(task.id))} />
-                </div>
-            </div>
-            <p className="mt-5">{task.description}</p>
+  return (
+    <div className="border px-5 py-3 rounded-md">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <div
+            className={cn("size-3 rounded-full", {
+              "bg-red-500": task.priority === "high",
+              "bg-yellow-500": task.priority === "medium",
+              "bg-green-500": task.priority === "low",
+            })}
+          ></div>
+          <h1 className={cn({ "line-through": task.isCompleted })}>
+            {task.title}
+          </h1>
         </div>
-    )
+        <div className="flex gap-3 items-center">
+
+          <Button
+            onClick={() => dispatch(deleteTask(task.id))}
+            variant="link"
+            className="p-0 text-red-500"
+          >
+            <Trash2 />
+          </Button>
+          <Checkbox
+            checked={task.isCompleted}
+            onClick={() => dispatch(toggleCompleteState(task.id))}
+          />
+        </div>
+      </div>
+      <p className="mt-5">{task.description}</p>
+    </div>
+  );
 }
--- redux-todo-app/src/pages/Tasks.tsx
+++ redux-todo-app/src/pages/Tasks.tsx
@@ -1,17 +1,27 @@
 import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/taskCard";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 export default function Tasks() {
   const tasks = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch()
 
   console.log(tasks);
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h1>Tasks</h1>
-        <AddTaskModal/>
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Tasks</h1>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger onClick={() => dispatch(updateFilter('all'))} value="all">All</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter('low'))} value="low">Low</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter('medium'))} value="medium">Medium</TabsTrigger>
+            <TabsTrigger onClick={() => dispatch(updateFilter('high'))} value="high">High</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
         {tasks.map((task) => (
